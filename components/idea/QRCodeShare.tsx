84239d79bdcde044225499fc0b127921250f52d6
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-03-24 00:29:58 +0900

    version4 - qr code share 수정

--- components/idea/QRCodeShare.tsx
+++ components/idea/QRCodeShare.tsx
@@ -5,7 +5,7 @@ export default function QRCodeShare() {
   return (
     <div className="mt-2 mb-6 flex justify-center">
       <QRCodeSVG 
-        value="http://gamehye.com" 
+        value="https://www.gamehye.com" 
         size={100}
         bgColor="transparent"
         fgColor="#e2e8f0" // tailwind gray-200 색상
